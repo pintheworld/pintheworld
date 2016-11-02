6b306fbb4bffcb3beaa7d8f6c724a48ed21839b8
Andreas Eliason <andreashg.eliasson@gmail.com>
2016-11-02 16:33:09 +0000

    fix: delete redundant main.js file

--- js-src/src/app/main.js
+++
@@ -1,9 +0,0 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-
-let boot = document.addEventListener('DOMContentLoaded', function() {
-  const platform = platformBrowserDynamic();
-  platform.bootstrapModule(AppModule);
-});
-
-module.exports = boot;
