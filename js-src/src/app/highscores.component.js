42d5acd1e5aceaefd07baacb6e78817898fa9d96
blanche <blanche@users.noreply.github.com>
2016-11-07 08:38:26 +0000

    Adding new components for #28
    implemented router for navigation between components
    implemented basic components for home, map and highscores
    aggregated components in main module

---
+++ js-src/src/app/highscores.component.js
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+import highscoresTemplate from './highscores.component.html'
+
+let HighscoresComponent = Component({
+    template: highscoresTemplate
+})
+    .Class({
+        constructor: function () {
+        }
+    });
+
+export {HighscoresComponent};
