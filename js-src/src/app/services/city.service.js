c3a299bcd60ab165b54816ac0bc364ec16ae441f
blanche <blanche@users.noreply.github.com>
2016-11-08 16:49:27 +0000

    modify example from Andreas to work with our backend & implement basic angular frontend binding

---
+++ js-src/src/app/services/city.service.js
@@ -0,0 +1,36 @@
+import {Class} from '@angular/core';
+import {Http} from '@angular/http';
+
+var CityService = Class({
+    constructor: [Http, function (http) {
+        this.http = http;
+        // using GAE
+        this.baseUrl = "https://pintheworld-146615.appspot.com/api/cities";
+        // using local development
+        // this.baseUrl = "http://localhost:8081/api/cities";
+    }],
+    getCities: function () {
+
+        return this.http.get(this.baseUrl).map(
+            function (res) {
+                return res.json()
+            }
+        )
+    },
+    createCity: function (name, lat, long) {
+        return this.http.post(this.baseUrl, {name, lat, long}).map(
+            function (res) {
+                return res.json()
+            }
+        )
+    },
+    deleteCities: function () {
+        return this.http.delete(this.baseUrl).map(
+            function (res) {
+                return res.json()
+            }
+        )
+    }
+});
+
+export {CityService};
