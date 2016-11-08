ec0a58ffbe6628246fabfc9c46359accc0c5949b
blanche <blanche@users.noreply.github.com>
2016-11-08 16:59:19 +0000

    add fix for windows problem "ImportError: No module named msvcrt"

--- python-src/appengine_config.py
+++ python-src/appengine_config.py
@@ -1,6 +1,12 @@
 """`appengine_config` gets loaded when starting a new application instance."""
 import sys
-import os.path
+import os
+
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+
+# fix for windows problem "ImportError: No module named msvcrt"
+if os.name == 'nt':
+    os.name = None
+    sys.platform = ''
