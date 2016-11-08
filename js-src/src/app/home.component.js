aaefde22b367f6ff3ff74ee6e808c450246e2e26
Andreas Eliasson <andreashg.eliasson@gmail.com>
2016-11-08 11:07:00 +0000

    example: how to inject and use services (#32)

--- js-src/src/app/home.component.js
+++ js-src/src/app/home.component.js
@@ -2,11 +2,30 @@ import {Component} from '@angular/core';
 
 import homeTemplate from './home.component.html'
 
+import {CountryService} from './country.service';
+
 let HomeComponent = Component({
-    template: homeTemplate
+    template: homeTemplate,
+    viewProviders: [CountryService]
 })
     .Class({
-        constructor: function () {
+        constructor: [CountryService, function (CountryService) {
+            this.CountryService = CountryService;
+        }],
+        getCountriesByRegion: function () {
+            var self = this;
+            self.countries = [];
+            self.error = '';
+            var region = 'oceania';
+            this.CountryService.getCountriesByRegion(region).subscribe(
+                function(data) {
+                    console.log(data);
+                    self.countries = data;
+                },
+                function (err) {
+                    self.error = 'we have an error';
+                }
+            );
         }
     });
 
