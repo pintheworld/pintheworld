65d8887e55518531401ae47e8dba4ad959a26f70
Xuan Xiang <stellaxxiang@gmail.ocm>
2016-11-14 19:16:34 +0000

    add getCities() for example, change infowindow

--- js-src/src/app/map.component.js
+++ js-src/src/app/map.component.js
@@ -9,37 +9,70 @@ import mapTemplate from './map.component.html';
 
 import '../../public/css/styles.css';
 import mapStyling from './map.component.css';
+import {CityService} from './services/city.service';
 
 import {GOOGLE_MAPS_DIRECTIVES,GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/esm/core';
 
 let MapComponent = Component({
     template: mapTemplate,
     styles: [mapStyling],
-    directives: [GOOGLE_MAPS_DIRECTIVES],
-    provider: [GOOGLE_MAPS_PROVIDERS]
+    directives: GOOGLE_MAPS_DIRECTIVES,
+    provider: GOOGLE_MAPS_PROVIDERS,
+    viewProviders: [CityService]
 })
 .Class({
-    constructor: function() {
+    constructor: [CityService, function(cityService) {
+        this.cityService = cityService;
+        this.error = '';
+        this.cities = [];
         this.markers = [];
         this.infoWindows = [];
-    },
+        
+        this.myName = 'Southampton';
+        this.myLat = 16;
+        this.myLong = 32;
+    }],
     mapClicked: function(e){
         if(this.markers.length === 0){
             this.markers.push({lat:e.coords.lat, lng:e.coords.lng});
             this.infoWindows.push({isOpen: 'true', details: 'Latitude: '+e.coords.lat.toFixed(6) + ', longitude: ' + e.coords.lng.toFixed(6) + '.'});
+            this.getCities();
+            console.log("im in mapclicked")
+            console.log(this.cities);
+            this.submitGuess(this.cities[0].name, e.coords.lat, e.coords.lng);
         }
         console.log('clicked');
     },
     clickedMarker: function(lat, lng, index) {
         console.log(`clicked the marker at: lat: ` + lat + ", lng: "+ lng + ". " + index);
     },
-    getCity: function(city){
-        return [{
-            "lat": city.lat,
-            "name": city.name,
-            "lng": city.lng,
-            "id": city.id
-        }];
+    getCities: function () {
+        var self = this;
+        this.cityService.getCities().subscribe(
+            function (data) {
+                console.log(data);
+                self.cities = data;
+            },
+            function (err) {
+                console.error(err);
+                this.error = 'we have an error';
+            }
+        );
+    },
+    createCity: function () {
+        var self = this;
+        this.cityService.createCity(this.myName, this.myLat, this.myLong).subscribe(
+            function (data) {
+                console.log(data);
+                self.getCities();
+            },
+            function (err) {
+                console.error(err);
+            }
+        );
+    },
+    submitGuess: function(cityName, userLat, userLng){
+        console.log('user guess: city: '+cityName+', userLat: '+userLat+', userlng: '+userLng);
     }
 });
 
